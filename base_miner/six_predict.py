c7661ca92e8e2356c56617125ed746aadfbe6c2f
phungkhaccuong <cuongst93@gmail.com>
2024-04-18 23:14:55 +0700

    u

--- base_miner/six_predict.py
+++ base_miner/six_predict.py
@@ -7,12 +7,17 @@ def predict():
     Predicting the next 6 candles using the last close
     """
     last_close = yf.Ticker('^GSPC').history(period='1s')['Close'].iloc[-1]
+    print(f"last_close::::{last_close}")
     df = yf.download('^GSPC', period='10m', interval='5m')
     diff = -(last_close - df.iloc[-1]['Close']) * 0.1
     diff_array = np.array([diff, -diff, diff, -diff, diff, -diff])
     pred_array = np.array([last_close for i in range(6)]) + diff_array
+    print(f"predict:::::::::::::::::{np.array([pred_array])}")
     return np.array([pred_array])
 
 
 if __name__ == "__main__":
     print(f'prediction: {predict()}')
+    data = predict()
+    final_data = list(data[0])
+    print(f"final_data:{final_data}")
